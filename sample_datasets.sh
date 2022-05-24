d4d3c5cef85bc8c721dbc1ec106331b603f6e4f7
austin2408 <sdnd93612@gmial.com>
2022-05-24 15:42:35 +0800

    download sample inference datasets

---
+++ sample_datasets.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+wget "https://drive.google.com/u/1/uc?id=1eF4i0yrxpTl9fV_UEzRoqNRNXHBjdWcA&export=download" -O "HANet_sample_datasets.zip"
+unzip -qq HANet_sample_datasets.zip
+rm -f HANet_sample_datasets.zip
