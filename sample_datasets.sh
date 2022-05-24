ffe8fe0db378f7775b03203c4b0cdd7bd23a59ff
austin2408 <sdnd93612@gmial.com>
2022-05-24 16:32:31 +0800

    update

--- sample_datasets.sh
+++ sample_datasets.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
-wget "https://drive.google.com/u/1/uc?id=1eF4i0yrxpTl9fV_UEzRoqNRNXHBjdWcA&export=download" -O "HANet_sample_datasets.zip"
+
+# HANet_sample_datasets
+echo "111111" | sudo -S gdown --id 1eF4i0yrxpTl9fV_UEzRoqNRNXHBjdWcA
 unzip -qq HANet_sample_datasets.zip
 rm -f HANet_sample_datasets.zip
+
+# HANet_datasets
+echo "111111" | sudo -S gdown --id 1Hx1UiaN_ezl82dM2C9EbVkM-2bPzqTRM
+unzip -qq HANet_datasets.zip
+rm -f HANet_datasets.zip
