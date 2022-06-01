af1e19eb5d1486f7bf16f1b96b3d4f7c06d3b5fb
austin2408 <sdnd93612@gmial.com>
2022-06-01 10:16:47 +0800

    download to data

--- sample_datasets.sh
+++ sample_datasets.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+cwd=$PWD
+
+if [ ! -d $cwd/data ]; then
+    mkdir data
+else
+    echo "folder exist"
+fi
+
+cd $cwd/data
+
 # HANet_sample_datasets
 echo "111111" | sudo -S gdown --id 1eF4i0yrxpTl9fV_UEzRoqNRNXHBjdWcA
 unzip -qq HANet_sample_datasets.zip
@@ -9,3 +19,5 @@ rm -f HANet_sample_datasets.zip
 echo "111111" | sudo -S gdown --id 1Hx1UiaN_ezl82dM2C9EbVkM-2bPzqTRM
 unzip -qq HANet_datasets.zip
 rm -f HANet_datasets.zip
+
+cd ..
