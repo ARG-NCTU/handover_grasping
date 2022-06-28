014c35f6b1103142b80c20fe72780fca92d0095e
austin2408 <sdnd93612@gmial.com>
2022-06-28 11:14:50 +0800

    add multi view sample data

--- sample_datasets.sh
+++ sample_datasets.sh
@@ -20,4 +20,9 @@ echo "111111" | sudo -S gdown --id 1Hx1UiaN_ezl82dM2C9EbVkM-2bPzqTRM
 unzip -qq HANet_datasets.zip
 rm -f HANet_datasets.zip
 
+# multi_view sample data
+echo "111111" | sudo -S gdown --id 1VIB6O_x1LrNNntmHGkhjvO_YBfaE-zx-
+unzip -qq multi_view.zip
+rm -f multi_view.zip
+
 cd ..
