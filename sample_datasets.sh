a9bf7b958efa99926f8a58701cab9b6ee358c126
austin2408 <sdnd93612@gmial.com>
2022-07-04 11:30:19 +0800

    update

--- sample_datasets.sh
+++ sample_datasets.sh
@@ -10,15 +10,25 @@ fi
 
 cd $cwd/data
 
-# HANet_sample_datasets
-echo "111111" | sudo -S gdown --id 1QEgHIFTS9L7Qo7TWBjhG_DCq3CLhENFl
-unzip -qq HANet_sample_datasets.zip
-rm -f HANet_sample_datasets.zip
-
 # HANet_datasets
 echo "111111" | sudo -S gdown --id 1Hx1UiaN_ezl82dM2C9EbVkM-2bPzqTRM
 unzip -qq HANet_datasets.zip
 rm -f HANet_datasets.zip
+
+# HANet testing datasets
+echo "111111" | sudo -S gdown --id 1yAckmX5aeo32iqwSPcyWx5uLlnqQTwzX
+unzip -qq HANet_datasets.zip
+rm -f HANet_datasets.zip
+
+# HANet Easy testing datasets
+echo "111111" | sudo -S gdown --id 1-eqnDHzbCyyjNqGo8mTQh3rAtHhfRhtm
+unzip -qq HANet_easy_datasets.zip
+rm -f HANet_easy_datasets.zip
+
+# DOPE testing datasets
+echo "111111" | sudo -S gdown --id 1X_x2ZghVudqd6Gdi1qzeBxW0V-VdWprm
+unzip -qq DOPE_testing.zip
+rm -f DOPE_testing.zip
 
 # multi_view sample data
 echo "111111" | sudo -S gdown --id 1VIB6O_x1LrNNntmHGkhjvO_YBfaE-zx-
