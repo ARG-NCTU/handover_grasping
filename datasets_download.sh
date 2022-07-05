9f0e160ee0c6285fa62e281fc38e8b1cc40cd806
austin2408 <sdnd93612@gmial.com>
2022-07-05 11:44:17 +0800

    add echo word

--- datasets_download.sh
+++ datasets_download.sh
@@ -10,32 +10,38 @@ fi
 
 cd $cwd/data
 
-# # HANet training datasets
+# HANet training datasets
+echo "Download HANet training datasets"
 echo "111111" | sudo -S gdown --id 1Hx1UiaN_ezl82dM2C9EbVkM-2bPzqTRM
 unzip -qq HANet_datasets.zip
 rm -f HANet_datasets.zip
 
 # HANet testing datasets
+echo "Download HANet testing datasets"
 echo "111111" | sudo -S gdown --id 1yAckmX5aeo32iqwSPcyWx5uLlnqQTwzX
 unzip -qq HANet_testing.zip
 rm -f HANet_testing.zip
 
-# # HANet Easy testing datasets
+# HANet Easy testing datasets
+echo "Download HANet Easy testing datasets"
 echo "111111" | sudo -S gdown --id 1-eqnDHzbCyyjNqGo8mTQh3rAtHhfRhtm
 unzip -qq HANet_easy_datasets.zip
 rm -f HANet_easy_datasets.zip
 
-# # DOPE testing datasets
+# DOPE testing datasets
+echo "Download  DOPE testing datasets"
 echo "111111" | sudo -S gdown --id 1X_x2ZghVudqd6Gdi1qzeBxW0V-VdWprm
 unzip -qq Dope_testing.zip
 rm -f Dope_testing.zip
 
-# # multi_view sample data
+# multi_view sample data
+echo "Download multi_view sample data"
 echo "111111" | sudo -S gdown --id 1VIB6O_x1LrNNntmHGkhjvO_YBfaE-zx-
 unzip -qq multi_view.zip
 rm -f multi_view.zip
 
-# # parallel-jaw-grasping-dataset
+# parallel-jaw-grasping-dataset
+echo "Download parallel-jaw-grasping-datase"
 echo "111111" | sudo -S gdown --id 1UKx5Bf0Wwg1RAr21UOBvibe03DcHvnwc
 unzip -qq parallel-jaw-grasping-dataset.zip
 rm -f parallel-jaw-grasping-dataset.zip.
--- model_download.sh
+++ model_download.sh
@@ -11,11 +11,13 @@ fi
 cd $cwd/model
 
 # Andy Zeng ConvNet
+echo "Download Andy Zeng ConvNet pretrained weight"
 echo "111111" | sudo -S gdown --id 1L1RUg2kz9nW5XwPdmHoxj7e3p9pDHVrH
 unzip -qq baseline-graspnet.zip
 rm -f baseline-graspnet.zip
 
 # DOPE pretrained-weight
+echo "Download DOPE pretrained weight"
 echo "111111" | sudo -S gdown --id 17bqzPhC47LpZhI4qD-fMgyzXm_tPv64X
 unzip -qq pretrained_weight.zip
 rm -f pretrained_weight.zip
