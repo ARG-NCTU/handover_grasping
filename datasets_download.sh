f997f6155a157595df0ca286b37c3369795b8da3
austin2408 <sdnd93612@gmail.com>
2022-08-15 14:21:15 +0800

    update link

--- datasets_download.sh
+++ datasets_download.sh
@@ -12,37 +12,38 @@ cd $cwd/data
 
 # HANet training datasets
 echo "Download HANet training datasets"
-echo "111111" | sudo -S gdown --id 1SOnUL_UmpuD2UY6Qkmtk0uc6O3A4KqRl
+echo "111111" | sudo -S gdown --id 1tBPq3BVQ7Pw5aGd2RkcazpSfcmXufsFG
 unzip -qq HANet_training_datasets.zip
 rm -f HANet_training_datasets.zip
 
 # HANet testing datasets
 echo "Download HANet testing datasets"
-echo "111111" | sudo -S gdown --id 1yAckmX5aeo32iqwSPcyWx5uLlnqQTwzX
+echo "111111" | sudo -S gdown --id 1YBSPzS4RS7ml5OGjDbKQA8TP3b_OmIlP
 unzip -qq HANet_testing.zip
 rm -f HANet_testing.zip
 
 # HANet Easy testing datasets
 echo "Download HANet Easy testing datasets"
-echo "111111" | sudo -S gdown --id 1-eqnDHzbCyyjNqGo8mTQh3rAtHhfRhtm
+echo "111111" | sudo -S gdown --id 1_ACfjnxUafW3v2oYk3NqSbKWTDPMJT4k
 unzip -qq HANet_easy_datasets.zip
 rm -f HANet_easy_datasets.zip
 
 # DOPE testing datasets
 echo "Download  DOPE testing datasets"
-echo "111111" | sudo -S gdown --id 1X_x2ZghVudqd6Gdi1qzeBxW0V-VdWprm
+echo "111111" | sudo -S gdown --id 1qA4yZcSXaHxOS_rIWArdiNgWrb8zp8CP
 unzip -qq Dope_testing.zip
 rm -f Dope_testing.zip
 
 # multi_view sample data
 echo "Download multi_view sample data"
-echo "111111" | sudo -S gdown --id 1VIB6O_x1LrNNntmHGkhjvO_YBfaE-zx-
+echo "111111" | sudo -S gdown --id 1P4w8N1KAgrAMKU1LY0_jFuTPZ7pIX6jn
 unzip -qq multi_view.zip
 rm -f multi_view.zip
 
 # parallel-jaw-grasping-dataset
 echo "Download parallel-jaw-grasping-datase"
-echo "111111" | sudo -S gdown --id 1UKx5Bf0Wwg1RAr21UOBvibe03DcHvnwc
+echo "111111" | sudo -S gdown --id 18UlspjFeJzrL7aKiBa0Bh2qHW3WdfHrK
+
 unzip -qq parallel-jaw-grasping-dataset.zip
 rm -f parallel-jaw-grasping-dataset.zip
 mv data parallel-jaw-grasping-dataset
